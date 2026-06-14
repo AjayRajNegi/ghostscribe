08264e1a731961068043567a5ebae42c8c19e6f2
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-06-14 10:45:33 +0530

    New UI

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,20 +4,25 @@ import { runCommit } from "./commit";
 import { render } from "ink";
 import React from "react";
 import { UserInput } from "../tui/test";
+import { Index } from "../tui";
 
 const program = new Command();
 
 program
   .name("ghostscribe")
   .description("AI-powered git commit message generator")
-  .version("0.1.0");
-
-program
-  .command("commit")
-  .description("Generate a commit message from staged changes")
-  .action(async (options) => {
-    const { waitUntilExit } = render(React.createElement(UserInput));
+  .version("0.1.0")
+  .action(async () => {
+    const { waitUntilExit } = render(React.createElement(Index));
     await waitUntilExit();
   });
+
+// program
+//   .command("ghostscribe")
+//   .description("Generate a commit message from staged changes")
+//   .action(async (options) => {
+//     const { waitUntilExit } = render(React.createElement(UserInput));
+//     await waitUntilExit();
+//   });
 
 program.parseAsync(process.argv);
